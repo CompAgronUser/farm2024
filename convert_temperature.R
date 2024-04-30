af28f6df1b17e378b6fab05132f8fccea6f580b3
CompAgronUser <101959973+CompAgronUser@users.noreply.github.com>
2024-04-30 11:24:37 +0800

    convert temperature F <-> C
    template only, wrong answer

---
+++ convert_temperature.R
@@ -0,0 +1,13 @@
+convert_temperature <- function(temperature, degree) {
+# Cheatsheet:
+#		answer <- 32 + temperature * 9 / 5
+#		answer <- (temperature - 32) * 5 / 9
+	if (degree == "F") {
+        answer <- temperature
+	} else if (degree == "C") {
+        answer <- temperature
+	} else {
+		answer <- NA
+	}
+	return(answer)
+}
