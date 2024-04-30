1d1d793bbe8afc5335d6fcf2079ad32aa89c155b
Steven Wu <stevenwu@ntu.edu.tw>
2024-04-30 14:04:52 +0800

    Update conversion
    - fix F to C

--- convert_temperature.R
+++ convert_temperature.R
@@ -3,7 +3,7 @@ convert_temperature <- function(temperature, degree) {
 #		answer <- 32 + temperature * 9 / 5
 #		answer <- (temperature - 32) * 5 / 9
 	if (degree == "F") {
-        answer <- 32 + temperature * 9 / 5
+            answer <- temperature * 9 / 5 * 32
 	} else if (degree == "C") {
         answer <- temperature
 	} else {
