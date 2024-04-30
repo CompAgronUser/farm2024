6e5c80facf5b4b61fbfbec4fbace7f1df4e9ba7a
Steven Wu <stevenwu@ntu.edu.tw>
2024-04-30 14:30:14 +0800

    add convert to degree_C

--- convert_temperature.R
+++ convert_temperature.R
@@ -5,7 +5,7 @@ convert_temperature <- function(temperature, degree) {
 	if (degree == "F") {
             answer <- temperature * 9 / 5 * 32
 	} else if (degree == "C") {
-        answer <- temperature
+            answer <- temperature - 32 * 5 / 9
 	} else {
 		answer <- NA
 	}
